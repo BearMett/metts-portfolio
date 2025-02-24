91800b52186e9a0315250533ceae958c421c0517
BearMett <agomdole@gmail.com>
2025-02-24 22:38:13 +0900

    📝 문서 내용와 크기, 레이아웃 수정

--- app/page.tsx
+++ app/page.tsx
@@ -38,12 +38,11 @@ export default function Home() {
           />
         </div>
         <div className="space-y-4">
-          <h1 className="text-4xl font-bold">김영민</h1>
-          <h2 className="text-2xl text-muted-foreground">시스템 및 서비스 개발자</h2>
+          <h1 className="text-4xl font-bold">김영민 | 시스템 및 서비스 개발자</h1>
           <p className="max-w-prose text-lg text-muted-foreground">
             안녕하세요. 함께 일하는 문화를 즐기는 소프트웨어 엔지니어 김영민입니다.
           </p>
-          <h3 className="max-w-prose text-lg text-muted-foreground">제가 일에 임하는 가치관은</h3>
+          <h2 className="text-2xl text-muted-foreground">제가 일에 임하는 가치관은</h2>
           <ul className="list-disc list-inside text-lg text-muted-foreground space-y-2">
             <li>사용자와 동료의 문제를 해결하고 만족시키는 데서 보람을 느낍니다.</li>
             <li>문제의 근본 원인을 파악하고 해결하고자 노력합니다.</li>
@@ -51,7 +50,7 @@ export default function Home() {
             <li>새로운 기술에 도전하되, 문제 해결의 수단으로 활용하는 것을 원칙으로 삼습니다.</li>
             <li>신중하게 설계하고 계획하되 과감하게 실행합니다.</li>
           </ul>
-          <h3 className="max-w-prose text-lg text-muted-foreground">이런 가치관을 토대로 업무에 임할 때</h3>
+          <h2 className="text-2xl text-muted-foreground">이런 가치관을 토대로 업무에 임할 때</h2>
           <ul className="list-disc list-inside text-lg text-muted-foreground space-y-2">
             <li>제가 작성한 코드나 API를 사용할 동료 역시 제 고객이라는 인식으로 사용법과 오류 상황을 문서화합니다.</li>
             <li>버그나 이슈 발생 시 단순 코드 수정에 그치지 않고 근본 원인을 분석하여 재발 방지책을 강구합니다.</li>
@@ -64,7 +63,7 @@ export default function Home() {
               것을 즐깁니다.
             </li>
           </ul>
-          <h3 className="max-w-prose text-lg text-muted-foreground">저의 강점은</h3>
+          <h2 className="text-2xl text-muted-foreground">저의 강점은</h2>
           <ul className="list-disc list-inside text-lg text-muted-foreground space-y-2">
             <li>
               모듈화와 단위 테스트를 적극 활용하여 코드를 작성합니다. 책임을 명확히 분리하고 재사용 가능한 코드를
